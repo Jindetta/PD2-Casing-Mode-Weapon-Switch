6f36bee22ddcdda497be8a0fe30a463fc8d4db3f
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-05-18 04:30:59 +0300

    Refractor player_inventory to inventory

--- Casing Mode Weapon Switch/main.lua
+++ Casing Mode Weapon Switch/main.lua
@@ -18,18 +18,18 @@ else
     end
 
     function HookClass:_check_change_weapon(...)
-        local player_inventory = self._ext_inventory
+        local inventory = self._ext_inventory
         local input = self:_get_input(...)
 
         if type(input.btn_primary_choice) == "number" then
-            player_inventory:equip_selection(input.btn_primary_choice)
+            inventory:equip_selection(input.btn_primary_choice)
         elseif type(self._previous_equipped_selection) == "number" then
-            player_inventory:equip_selection(self._previous_equipped_selection)
+            inventory:equip_selection(self._previous_equipped_selection)
             self._previous_equipped_selection = nil
         elseif input.btn_switch_weapon_press then
-            player_inventory:equip_next()
+            inventory:equip_next()
         end
 
-        player_inventory:hide_equipped_unit()
+        inventory:hide_equipped_unit()
     end
 end
